5f2cefae2ad9f5ebf6f1bb2474a44e4f0220d657
kermitemperor <akospalviscsak@gmail.com>
2026-07-14 21:05:13 +0200

    productive bees GREENLIT BOOOISSSS

---
+++ kubejs/server_scripts/prod_bees.js
@@ -0,0 +1,34 @@
+ServerEvents.recipes(event => {
+    event.remove({id: 'productivebees:bee_produce/alloys/steel_bee'})
+    event.remove({id: 'productivebees:centrifuge/alloys/honeycomb_steel'})
+
+    event.remove({id: 'productivebees:bee_produce/alloys/electrum_bee'})
+    event.remove({id: 'productivebees:centrifuge/alloys/honeycomb_electrum'})
+
+    event.remove({id: 'productivebees:bee_produce/alloys/bronze_bee'})
+    event.remove({id: 'productivebees:centrifuge/alloys/honeycomb_bronze'})
+
+    event.remove({id: 'productivebees:bee_produce/aquaculture/neptunium_bee'})
+    event.remove({id: 'productivebees:centrifuge/aquaculture/honeycomb_neptunium'})
+
+    event.remove({id: 'productivebees:bee_produce/chemlib/neptunium_bee'})
+    event.remove({id: 'productivebees:centrifuge/chemlib/honeycomb_neptunium'})
+
+    event.remove({id: 'productivebees:bee_produce/gems/diamond_bee'})
+    event.remove({id: 'productivebees:centrifuge/gems/honeycomb_diamond'})
+
+    event.remove({id: 'productivebees:bee_produce/gems/emerald_bee'})
+    event.remove({id: 'productivebees:centrifuge/gems/honeycomb_emerald'})
+
+    event.remove({id: 'productivebees:bee_produce/gems/rose_quartz_bee'})
+    event.remove({id: 'productivebees:centrifuge/gems/honeycomb_rose_quartz'})
+
+    event.remove({id: 'productivebees:bee_produce/raw_materials/netherite_bee'})
+    event.remove({id: 'productivebees:centrifuge/raw_materials/honeycomb_netherite'})
+
+    event.remove({id: 'productivebees:bee_produce/raw_materials/netherite_bee'})
+    event.remove({id: 'productivebees:centrifuge/raw_materials/honeycomb_netherite'})
+
+    event.remove({id: 'productivebees:bee_produce/alloys/brass_bee'})
+    event.remove({id: 'productivebees:centrifuge/alloys/honeycomb_brass'})
+})
